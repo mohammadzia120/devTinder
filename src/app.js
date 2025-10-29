7464196f062c9c4c12cedae893e9555aed0a0027
ziaNizami <mohammadzia120@gmail.com>
2025-10-29 09:59:39 +0530

    Created server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("hello from 3000");
+});
+app.listen(3000, () => {
+  console.log(`server is running at 3000`);
+});
